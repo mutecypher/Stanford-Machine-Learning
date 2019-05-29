6b277873d81fdc0c2c5490bb549ca488a6bd0aaa
mutecypher <mutecypher@icloud.com>
2019-05-29 13:11:49 -0700

    Update ex1_multi.m

--- ex1/ex1_multi.m
+++ ex1/ex1_multi.m
@@ -51,9 +51,14 @@ fprintf('Normalizing Features ...\n');
 
 [X mu sigma] = featureNormalize(X);
 
+
 % Add intercept term to X
 X = [ones(m, 1) X];
 
+
+X = [ones(m, 1), data(:,1)]; % Add a column of ones to x
+theta = zeros(2, 1); % initialize fitting parameters
+fprintf(' x = [%.0f %.0f], y = %.0f \n', [X(1:10,:) y(1:10,:)]');
 
 %% ================ Part 2: Gradient Descent ================
 
@@ -82,8 +87,8 @@ X = [ones(m, 1) X];
 fprintf('Running gradient descent ...\n');
 
 % Choose some alpha value
-alpha = 0.01;
-num_iters = 400;
+alpha = 1;
+num_iters = 50;
 
 % Init Theta and Run Gradient Descent 
 theta = zeros(3, 1);
@@ -104,8 +109,8 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-predict1 = [1650, 3] *theta;
-price = predict1; % You should change this
+
+price = price = [1 (1650-mu(1))/sigma(1) (3-mu(2))/sigma(2)]*theta; % You should change this
 
 
 % ============================================================
@@ -150,8 +155,8 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-predict1 = [1650,3] * theta;
-price = 331000; % You should change this
+ 
+price = price = [1 1650 3]*theta ;% You should change this
 
 
 % ============================================================

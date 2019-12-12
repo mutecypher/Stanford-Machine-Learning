46aadb8ee77e142a0a295b6b0d6673c21ba931bf
mutecypher <mn3osyn3@gmail.com>
2019-12-11 18:07:36 -0800

    yow

--- ex1/ex1_multi.m
+++ ex1/ex1_multi.m
@@ -51,6 +51,7 @@ fprintf('Normalizing Features ...\n');
 
 [X mu sigma] = featureNormalize(X);
 
+
 % Add intercept term to X
 X = [ones(m, 1) X];
 
@@ -82,8 +83,8 @@ X = [ones(m, 1) X];
 fprintf('Running gradient descent ...\n');
 
 % Choose some alpha value
-alpha = 0.01;
-num_iters = 400;
+alpha = 1;
+num_iters = 50;
 
 % Init Theta and Run Gradient Descent 
 theta = zeros(3, 1);
@@ -104,7 +105,8 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
+
+price = price = [1 (1650-mu(1))/sigma(1) (3-mu(2))/sigma(2)]*theta; % You should change this
 
 
 % ============================================================
@@ -149,7 +151,8 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
+ 
+price = price = [1 1650 3]*theta ;% You should change this
 
 
 % ============================================================
